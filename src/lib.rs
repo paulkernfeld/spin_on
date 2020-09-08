d87a8cf2a6b8f8c5e544ca9333bc40c702bdf3c3
paulkernfeld <me@paulkernfeld.com>
2020-09-08 09:47:17 -0400

    add docs and another test

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
+//! This crate contains what aims to be the simplest possible implementation of a valid executor.
+//! Instead of nicely parking the thread and waiting for the future to wake it up, it continuously
+//! polls the future until the future is ready. This will probably use a lot of CPU, so be careful
+//! when you use it.
+//!
+//! The advantages of this crate are:
+//!
+//! - It is really simple
+//! - It has no dependency on `std` or on an allocator
+//! - It only has one dependency
 #![no_std]
 
 use core::future::Future;
-use core::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
 use core::sync::atomic::spin_loop_hint;
+use core::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
 
 // TODO audit that this noop waker implementations aren't doing anything bad
 unsafe fn rwclone(_p: *const ()) -> RawWaker {
@@ -33,7 +43,7 @@ pub fn spin_on<F: Future>(future: F) -> F::Output {
     let mut cx = Context::from_waker(waker);
     loop {
         if let Poll::Ready(output) = future.as_mut().poll(&mut cx) {
-            spin_loop_hint();
+            spin_loop_hint(); // TODO am I using this correctly?
             return output;
         }
     }
@@ -41,8 +51,33 @@ pub fn spin_on<F: Future>(future: F) -> F::Output {
 
 #[cfg(test)]
 mod tests {
+    use core::future::Future;
+    use core::pin::Pin;
+    use core::task::{Context, Poll};
+
+    struct CountFuture(usize);
+
+    impl Future for CountFuture {
+        type Output = ();
+
+        fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+            if self.0 > 0 {
+                self.0 -= 1;
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            } else {
+                Poll::Ready(())
+            }
+        }
+    }
+
     #[test]
     fn ready() {
         crate::spin_on(async {});
+    }
+
+    #[test]
+    fn count() {
+        crate::spin_on(CountFuture(10));
     }
 }
