c2fb78a05dc6dffe641be73f5bf62aec7be88016
not_a_seagull <notaseagull048@gmail.com>
2021-01-14 21:00:43 -0800

    Should probably also update the libdoc

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@
 //! - It is really simple
 //! - It should work on basically any platform
 //! - It has no dependency on `std` or on an allocator
-//! - It only has one dependency
+//! - It has no dependencies
 //!
 //! ## The Design
 //!
